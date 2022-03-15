0a12c95b1542e76d0cca0548325be825e8e13a17
Johnson <weihsun.chen@nogle.com>
2022-03-15 18:07:48 +0800

    add api key

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -3,7 +3,10 @@ import axios from 'axios';
 const API_PATH = 'https://api.themoviedb.org/3/';
 
 const apiInstance = axios.create({
-  baseURL: API_PATH
+  baseURL: API_PATH,
+  headers: {
+    'api_key': process.env['API_TOKEN'] || ''
+  } 
 });
 
 export default apiInstance;
